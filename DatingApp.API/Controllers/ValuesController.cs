209a93710b402283a25ee586fa60ca2cd7e06291
Oleksandr Yankivskyy <oleksandr.yankivskyy@siemens.com>
2019-10-14 17:49:19 +0100

    End of Section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace DatingApp.API.Controllers
             _context = context;
         }
 
-        //Get api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
